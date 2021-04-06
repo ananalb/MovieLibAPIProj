3ec7111c606a12e3d92a030e1b72e8ec3d1b705f
unknown <ana.nalbandyan@gmail.com>
2021-04-06 14:08:46 -0700

    working on edit

--- Client/app.js
+++ Client/app.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
             
             $.each(data, function(index, el){               
                $('#movieData')
-               .append(`<tr><td>${el.title}</td>q
+               .append(`<tr>
+               <td>${el.title}</td>
                <td>${el.genre}</td>
                <td>${el.director}</td>
                <td><button onclick='deleteMovie(${el.movieId})'>Delete</button></td>
@@ -39,11 +40,6 @@ function deleteMovie(id){
             }            
         });
     }
-   
-    //$('#my-form').submit( processForm);
-    
-    
-
 
 function processForm( e ){
     var dict = {
@@ -71,24 +67,24 @@ function processForm( e ){
 }
 $('#my-form').submit( processForm);
 
-
- /*    
-function processFormEdit( movieId ){
+    
+function editMovie(id){
     var dict = {
-        Title : this["title"].val(),
-        Genre : this["genre"].val(),
-        Director: this["director"].val()
+        Title : this["title"].value,
+        Genre : this["genre"].value,
+        Director: this["director"].value
     };
     $.ajax({
-        url: 'https://localhost:44325/api/movie',
+        url: 'https://localhost:44325/api/movie/' + id,
         dataType: 'json',
         type: "PUT",
         contentType: 'application/json',
         data: JSON.stringify(dict),
-        success: function( data, textStatus, jQxhr ){                  
-                    $('#movieData').update(data);
-                    $('td',movieId).each(function() {
+        success: function(id){            
+                    $('#movieData').update(id);
+                    $('td',id).each(function() {
     $(this).html('<input type="text" value="' + $(this).html() + '" />');
+    addRow();
 })
         },
         error: function( jqXhr, textStatus, errorThrown ){
@@ -97,31 +93,45 @@ function processFormEdit( movieId ){
         }
         
     });
-    movieId.preventDefault();
+    id.preventDefault();
 }
-$('#my-edit-form').submit( processFormEdit);
- 
-    
-  
-
-  */
- 
+$('#my-form').submit( editMovie);
 
 
 
-
-// function deleteMovie(id) {
-//     $.ajax({
-//         url: 'https://localhost:44325/api/movie/' + id,
-//         type: 'DELETE',
-//         success: function () {
-//             alert ("record has been deleted");
-          
-//         },
-//         error:function (err) {
-//             alert(err);
-//         }
-        
-//     });
+// $(document.createElement('table'))
+// function createTable{
+//     tb = document.createElement("tbody")  
+//     var tbody  = document.createElement('tbody'); 
+//     table.appendChild(tbody);
+//     var table_row  = document.createElement('tr'); 
+//     tbody.appendChild(table_row)
 // }
+
+
+function addRow(tableID) {
+
+    
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    var cell1 = row.insertCell(0);
+    var element1 = document.createElement("input");
+    element1.type = "checkbox";
+    element1.name="chkbox[]";
+    cell1.appendChild(element1);
+
+    var cell2 = row.insertCell(1);
+    cell2.innerHTML = rowCount + 1;
+
+    var cell3 = row.insertCell(2);
+    var element2 = document.createElement("input");
+    element2.type = "text";
+    element2.name = "txtbox[]";
+    cell3.appendChild(element2);
+
+
+}
+
 
